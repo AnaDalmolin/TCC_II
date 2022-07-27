695137a37576b522a7729613b4915513232824da
AnaDalmolin <55006937+AnaDalmolin@users.noreply.github.com>
2022-07-27 15:59:11 -0300

    004-CriaçãoFunçãoLogin/Cadastro

--- lib/src/bloc/cadastro.dart
+++ lib/src/bloc/cadastro.dart
@@ -1,8 +1,9 @@
 
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:tcc_ll/src/data/user_db.dart';
-import 'package:tcc_ll/src/models/user_model.dart';
+import 'package:flutter/material.dart';
+// import 'package:tcc_ll/src/models/user_model.dart';
 
 abstract class Cadastro {
 
@@ -11,10 +12,35 @@ abstract class Cadastro {
   // Function(List<PaisModel>) get changeListPaises => pageController.sink.add;
   // List<PaisModel> get listPaises => pageController.value;
 
-  cadastrar(nome, email, senha, telefone, data, responsavel){
+
+  // CRIAR USUÁRIO PELA TABELA DE BANCO DE DADOS.
+  // cadastrar(nome, email, senha, telefone, data, responsavel){
     
-    final newUser = User(1 ,nome, email, senha, telefone, data, responsavel);
-    UserDB().save(newUser).then((value) => print(value));
+  //   final newUser = User(1 ,nome, email, senha, telefone, data, responsavel);
+  //   UserDB().save(newUser).then((value) => print(value));
+  // }
+
+  //CRIANDO PELO FIREBASE
+  void register(email, password) async{
+    try {
+      UserCredential user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+      if(email != null && password != null){
+        //NAVEGAR PARA PÁGINA DE DASHBOARD
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+  
+  void login(email, password) async{
+    try {
+      UserCredential user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      if(email != null){
+        //NAVEGAR PARA PÁGINA DE DASHBOARD
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
