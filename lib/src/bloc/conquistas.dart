da48574324e3ad7627cfc43ce28788a335142d24
ana <anadv21@hotmai.com>
2022-10-25 21:22:16 -0300

    032-CriandoFuncaoConquistas

--- lib/src/bloc/conquistas.dart
+++ lib/src/bloc/conquistas.dart
@@ -2,7 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:quickalert/quickalert.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-final CollectionReference _mainCollection = _firestore.collection('Conquistas');
+final CollectionReference _mainCollection =
+    _firestore.collection('ListaConquistas');
 
 class ConquistasBloc {
   static String? userUid;
@@ -11,18 +12,17 @@ class ConquistasBloc {
   static Future<void> addItem({
     required String nome,
     required double valor,
-    required double deposito,
-    required String descricao,
+    required String fraseConquista,
+    required String indentificador,
     required String userId,
   }) async {
     DocumentReference documentReferencer =
-        _mainCollection.doc(userId).collection('Objetivos').doc();
+        _mainCollection.doc(userId).collection('Conquistas').doc();
 
     Map<String, dynamic> data = <String, dynamic>{
       "nome": nome,
       "valor": valor,
-      "deposito": deposito,
-      "descricao": descricao,
+      "descricao": fraseConquista,
     };
 
     await documentReferencer
@@ -30,74 +30,104 @@ class ConquistasBloc {
         // ignore: avoid_print
         .whenComplete(() => print("Note item added to the database"))
         // ignore: avoid_print
-        .catchError((e) => print(e));
-  }
-
-// ADD ITEM
-  static Future<void> addObjetivoConcluido({
-    required String nome,
-    required double valor,
-    required String userId,
-  }) async {
-    DocumentReference documentReferencer =
-        _mainCollection.doc(userId).collection('ObjetivosConcluido').doc();
-
-    Map<String, dynamic> data = <String, dynamic>{
-      "nome": nome,
-      "valor": valor,
-    };
-
-    await documentReferencer
-        .set(data)
-        // ignore: avoid_print
-        .whenComplete(() => print("Note item added to the database"))
-        // ignore: avoid_print
-        .catchError((e) => print(e));
-  }
-
-// UPDATE ITEM
-  static Future<void> updateItem({
-    required String nome,
-    required double valor,
-    required double deposito,
-    required String descricao,
-    required String userId,
-    required String docId,
-  }) async {
-    DocumentReference documentReferencer =
-        _mainCollection.doc(userId).collection('Objetivos').doc(docId);
-
-    Map<String, dynamic> data = <String, dynamic>{
-      "nome": nome,
-      "valor": valor,
-      "deposito": deposito,
-      "descricao": descricao,
-    };
-
-    await documentReferencer
-        .update(data)
-        .whenComplete(() => print("Note item updated in the database"))
         .catchError((e) => print(e));
   }
 
 // READ ITEM
   static Stream<QuerySnapshot> readItems({userId}) {
     CollectionReference notesItemCollection =
-        _mainCollection.doc(userId).collection('Objetivos');
+        _mainCollection.doc(userId).collection('Conquistas');
 
     return notesItemCollection.snapshots();
   }
 
-// DELETE ITEM
+  // 1- Conquista
+  // ignore: non_constant_identifier_names
+  DepositoInicial({valor, userId, context}) async {
+    await addItem(
+        nome: "Primeiro deposito a gente nunca esquece!",
+        valor: valor,
+        fraseConquista: "AEEE, continue assim! :D",
+        userId: userId,
+        indentificador: "primeiroDeposito1");
 
-  static Future<void> deleteItem(
-      {required String docId, required userId}) async {
-    DocumentReference documentReferencer =
-        _mainCollection.doc(userId).collection('Objetivos').doc(docId);
+    return QuickAlert.show(
+      context: context,
+      type: QuickAlertType.success,
+      title: 'Conquista Realizada: Primeiro deposito a gente nunca esquece!',
+      text: 'AEEE, continue assim! :D',
+    );
+  }
 
-    await documentReferencer
-        .delete()
-        .whenComplete(() => print('Note item deleted from the database'))
-        .catchError((e) => print(e));
+  // 2- Conquista
+  // ignore: non_constant_identifier_names
+  PrimeiroObjetivo({valor, userId, context}) async {
+    await addItem(
+        nome: "Criar um Objetivo!",
+        valor: valor,
+        fraseConquista: "Pronto! Seu objetivo foi criado! :D",
+        userId: userId,
+        indentificador: "PrimeiroObjetivo");
+
+    return QuickAlert.show(
+      context: context,
+      type: QuickAlertType.success,
+      title: 'Conquista Realizada: Criar um Objetivo!',
+      text: 'Pronto! Seu objetivo foi criado! :D',
+    );
+  }
+
+  // 3- Conquista
+  // ignore: non_constant_identifier_names
+  FinalizarObjetivo({valor, userId, context}) async {
+    await addItem(
+        nome: "Finalizar um Objetivo!",
+        valor: valor,
+        fraseConquista: "Isso aee, objetivo finalizado! :D",
+        userId: userId,
+        indentificador: "FinalizarObjetivo");
+
+    return QuickAlert.show(
+      context: context,
+      type: QuickAlertType.success,
+      title: 'Conquista Realizada: Finalizar um Objetivo!',
+      text: 'Isso aee, objetivo finalizado! :D',
+    );
+  }
+
+  // 4- Conquista
+  // ignore: non_constant_identifier_names
+  DepositoRealizado({valor, userId, context}) async {
+    await addItem(
+        nome: "Deposito Realizado!",
+        valor: valor,
+        fraseConquista: "Isso aee, seu deposito foi realizado! :D",
+        userId: userId,
+        indentificador: "DepositoRealizado");
+
+    return QuickAlert.show(
+      context: context,
+      type: QuickAlertType.success,
+      title: 'Conquista Realizada: Deposito Realizado!',
+      text: 'Isso aee, seu deposito foi realizado! :D',
+    );
+  }
+
+  // 5- Conquista
+  // ignore: non_constant_identifier_names
+  SaqueRealizado({valor, userId, context}) async {
+    await addItem(
+        nome: "Saque Realizado!",
+        valor: valor,
+        fraseConquista: "Cuidado pra não gastar muito! Brincadeirinha hehe :D",
+        userId: userId,
+        indentificador: "DepositoRealizado");
+
+    return QuickAlert.show(
+      context: context,
+      type: QuickAlertType.success,
+      title: 'Conquista Realizada: Saque Realizado!',
+      text: 'Cuidado pra não gastar muito! Brincadeirinha hehe :D',
+    );
   }
 }
